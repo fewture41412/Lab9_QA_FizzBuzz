07e1d49a9b3a5f21a4b086a59b250a2283588d53
“fewture41412” <few414122541@gmail.com>
2019-10-09 14:24:39 +0700

    fizzbuzz input = 8

--- src/FizzBuzz/fizzbuzz/src/fizzbuzz/Fizzbuzz.java
+++ src/FizzBuzz/fizzbuzz/src/fizzbuzz/Fizzbuzz.java
@@ -14,45 +14,41 @@ class Fizzbuzz {
 	void input1() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("1",fizzBuzz.fizzBuzz(1));
-		}
+	}
 
 	@Test
 	@DisplayName ("Tesing FizzBuzz input =2")
 	void input2() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("2",fizzBuzz.fizzBuzz(2));
-		}
+	}
 	
 	@Test
 	@DisplayName ("Tesing FizzBuzz input =3")
 	void input3() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("fizz",fizzBuzz.fizzBuzz(3));
-		}
+	}
 		
 	@Test
 	@DisplayName ("Tesing FizzBuzz input =4")
 	void input4() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("4",fizzBuzz.fizzBuzz(4));
-		}
+	}
 	
 	@Test
 	@DisplayName ("Tesing FizzBuzz input =5")
 	void input5() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("buzz",fizzBuzz.fizzBuzz(5));
-		}
+	}
 		
 	@Test
 	@DisplayName ("Tesing FizzBuzz input =6")
 	void input6() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("fizz",fizzBuzz.fizzBuzz(6));
-		}
-
-		
-		
 	}
 	
 	@Test
@@ -60,7 +56,14 @@ class Fizzbuzz {
 	void input7() {
 		lab9tester fizzBuzz = new lab9tester();		
 		assertEquals("7",fizzBuzz.fizzBuzz(7));
-		}
+	}
+	
+	@Test
+	@DisplayName ("Tesing FizzBuzz input =8")
+	void input8() {
+		lab9tester fizzBuzz = new lab9tester();		
+		assertEquals("8",fizzBuzz.fizzBuzz(8));
+	}
 
 		
 		
